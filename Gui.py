70176a55a0eeeccd9c94f8da098521cb84acd588
Arvi Lintervo <arvi.lintervo@aalto.fi>
2020-03-23 19:42:37 +0200

    Added button to change the colors, started to implement player-class and stuff

--- Gui.py
+++ Gui.py
@@ -15,6 +15,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QBrush, QPen
 from PyQt5.QtWidgets import QWidget, QApplication, QGridLayout, QPushButton
 from color import ColorItem
+from player import Player
 import random
 
 
@@ -27,6 +28,7 @@ class GUI(QWidget):
         self.sceneHeight = 500
         super().__init__()
         self.init_window()
+        self.player1 = Player(1)
         
         #self.timer = QtCore.QTimer()
         #self.timer.setInterval(10)
@@ -43,15 +45,18 @@ class GUI(QWidget):
                 
     def init_window(self):
         #Set up the window
-        self.setFixedSize(1000, 500)
+        self.setFixedSize(1000, 600)
         self.setWindowTitle('Colour capture')
         self.setStyleSheet("QWidget { background: #aeddf9 }") 
         
         #set up scenes
-        self.scene = QtWidgets.QGraphicsScene()
-        self.scene.setSceneRect(0, 0, self.get_width(), self.get_height())
+        self.mainScene = QtWidgets.QGraphicsScene()
+        self.mainScene.setSceneRect(0, 0, 1000, 600)
+
+        self.controlScene = QtWidgets.QGraphicsScene()
+        self.controlScene.setSceneRect(self.get_width(), 0, 1000-self.get_width(), self.get_height())
         
-        self.view = QtWidgets.QGraphicsView(self.scene, self)
+        self.view = QtWidgets.QGraphicsView(self.mainScene, self)
         self.view.setFocusPolicy(QtCore.Qt.NoFocus)
         self.view.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.view.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
@@ -59,32 +64,48 @@ class GUI(QWidget):
 
         self.brush = QtGui.QBrush(Qt.SolidPattern)
         self.pen = QtGui.QPen(Qt.SolidLine)
+         
         
-        
-        tmp = []
-        
-        for i in range(0,self.get_width(), 20):
-            for j in range(0,self.get_height(), 20):
-                tmp.append(ColorItem(i, j, 20, 20))
-            self.colors.append(tmp)
-            tmp = []
-
+        self.init_color_items()
         self.init_colors()
-
+        self.paint_colors()
+        self.init_buttons()
         
-        for n in self.colors:
-            for m in n:
-                self.brush.setColor(m.get_color())
-                self.scene.addRect(m.get_rect_item(), self.pen, self.brush)
             
         self.show()
         self.view.show()        
 
         
+    def init_color_items(self):
+        tmp = []
+        for i in range(0,self.get_height(), 20):
+            for j in range(0,self.get_width(), 20):
+                tmp.append(ColorItem(j, i, 20, 20))
+            self.colors.append(tmp)
+            tmp = []
 
     def init_colors(self):
         for n in self.colors:
             for m in n:
                 m.set_color(random.randint(0,3))
+
+    def paint_colors(self):
+        for n in self.colors:
+            for m in n:
+                self.brush.setColor(m.get_color())
+                self.mainScene.addRect(m.get_rect_item(), self.pen, self.brush)
+
+
+    def change_colors(self):
+        self.init_colors()
+        self.paint_colors()
                 
-        
+    def init_buttons(self):
+        self.button = QPushButton("Change colors")
+        self.button.resize(200,100)
+        self.button.move(self.get_width(), 0)
+        self.button.clicked.connect(self.change_colors) 
+        self.mainScene.addWidget(self.button)
+
+
+    
---
+++ player.py
@@ -0,0 +1,33 @@
+
+
+
+import sys
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QApplication, QGridLayout, QPushButton
+
+colors = [Qt.red, Qt.green, Qt.cyan, Qt.magenta]
+
+class Player():
+
+    def __init__(self, number):
+        self.color = colors[number]
+        self.number = number
+        self.area = []
+        self.init_own_area()
+
+
+    def init_own_area(self):
+        tmp = []
+        for i in range(0,500, 20):
+            for j in range(0,800, 20):
+                tmp.append(0)
+            self.area.append(tmp)
+            tmp = []
+
+        if (self.number == 1):
+            self.area[len(self.area)-1][0] = 1
+        else:
+            self.area[0][len(self.area[0])-1] = 1
+
+        print(self.area)
